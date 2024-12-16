784aa9ad177075a63fce46f213c7729d1ab312fe
saba gogberashvili <sgogberashvili@gmail.com>
2024-12-16 17:16:53 +0400

    first commit

--- Shop/products/urls.py
+++ Shop/products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/<int:id>/', views.product_detail, name='product_detail'),
+    path('product/<int:id>', views.product_detail, name='product_detail'),
 ]
--- Shop/products/views.py
+++ Shop/products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+
 def home(request):
-    products = Product.objects.all()
+    sort_by = request.GET.get('sort', 'name')
+    products = Product.objects.all().order_by(sort_by)
     return render(request, 'home.html', {'products': products})
+
 
 def product_detail(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'product_detail.html', {'product': product})
-
 
